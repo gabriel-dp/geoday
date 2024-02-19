a7fde6ea793748ec8a00508a5016899c47a81a0b
gabriel-dp <gabriel.meira.2004@gmail.com>
2024-02-19 14:28:15 -0300

    :sparkles: Initial statistics popup content

--- src/pages/Home/Header/Statistics/index.tsx
+++ src/pages/Home/Header/Statistics/index.tsx
@@ -1,13 +1,30 @@
 import useLanguage from "@/contexts/language/useLanguage";
+import useGame from "@/contexts/game/useGame";
 
 import { StatisticsContainer } from "./styles";
 
+function Registry(props: { title: string; children: React.ReactNode }) {
+	return (
+		<div className="registry">
+			<p>{props.title}</p>
+			{props.children}
+		</div>
+	);
+}
+
 export default function Statistics() {
 	const { t } = useLanguage();
+	const { usedHints, usedMap } = useGame().daily;
 
 	return (
 		<StatisticsContainer>
 			<h1>{t`popups.statistics`}</h1>
+			<Registry title={t`statistics.usedHints`}>
+				<p>{t(`boolean.${usedHints.toString()}` as const)}</p>
+			</Registry>
+			<Registry title={t`statistics.usedMap`}>
+				<p>{t(`boolean.${usedMap.toString()}` as const)}</p>
+			</Registry>
 		</StatisticsContainer>
 	);
 }
--- src/pages/Home/Header/Statistics/styles.ts
+++ src/pages/Home/Header/Statistics/styles.ts
@@ -4,4 +4,10 @@ export const StatisticsContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	gap: 1rem;
+
+	.registry {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
 `;
